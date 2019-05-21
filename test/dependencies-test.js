af977ca999db965b57382af45772abca15e09bf3
Chris Eppstein <chris@eppsteins.net>
2019-05-21 11:46:58 -0700

    Use content hashing to detect changes to local files (files in the broccoli tree), keep using timestamps for external files.

--- test/dependencies-test.js
+++ test/dependencies-test.js
@@ -27,9 +27,20 @@ function pathFor(root, filePath) {
 
 /**
  * @param filePath {string}
+ * @param [contents] {string}
  */
-function touch(filePath) {
-  fs.closeSync(fs.openSync(filePath, 'w'));
+function touch(filePath, contents) {
+  if (contents) {
+    let fd = fs.openSync(filePath, 'a');
+    if (contents) {
+      fs.writeSync(fd, contents);
+    }
+    fs.closeSync(fd);
+  } else {
+    let stats = fs.statSync(filePath);
+    fs.utimesSync(filePath, stats.atime, new Date());
+    stats = fs.statSync(filePath);
+  }
 }
 
 describe('Dependency Invalidation', function() {
@@ -82,7 +93,7 @@ describe('Dependency Invalidation', function() {
       pathFor('subdirFile2.txt')
     ]);
     dependencies.seal();
-    let deps = dependencies.allDependencies.get(FS_ROOT);
+    let deps = dependencies.allDependencies.get(DEP_FIXTURE_DIR);
     assert.equal(deps.size, 1);
   });
 
@@ -98,7 +109,9 @@ describe('Dependency Invalidation', function() {
     ]);
     dependencies.seal();
     let deps = dependencies.allDependencies.get(FS_ROOT);
-    assert.equal(deps.size, 3);
+    assert.equal(deps.size, 1);
+    let localdeps = dependencies.allDependencies.get(DEP_FIXTURE_DIR);
+    assert.equal(localdeps.size, 2);
     let dependents = dependencies.dependentsMap.get(pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile1.txt'));
     assert.deepEqual(dependents, [pathFor('file1.txt')]);
     dependents = dependencies.dependentsMap.get(pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile2.txt'));
@@ -120,63 +133,83 @@ describe('Dependency Invalidation', function() {
     dependencies.seal();
     dependencies.captureDependencyState();
     let fileEntries = dependencies.fsTrees.get(FS_ROOT).entries.filter((e) => !e.isDirectory());
-    assert.equal(3, fileEntries.length);
+    assert.equal(1, fileEntries.length);
+    let localFileEntries = dependencies.fsTrees.get(DEP_FIXTURE_DIR).entries.filter((e) => !e.isDirectory());
+    assert.equal(2, localFileEntries.length);
     let paths = fileEntries.map(e => path.resolve(FS_ROOT, e.relativePath));
     assert.deepEqual(paths.sort(), [
+      pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt')
+    ].sort())
+    let localPaths = localFileEntries.map(e => path.resolve(DEP_FIXTURE_DIR, e.relativePath));
+    assert.deepEqual(localPaths.sort(), [
       pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile1.txt'),
       pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile2.txt'),
-      pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt')
     ].sort())
   });
 
   it('can compute invalidations', function () {
-    let dependencies = new Dependencies(DEP_FIXTURE_DIR);
-    dependencies.setDependencies(pathFor('file1.txt'), [
-      pathFor('subdir/subdirFile1.txt'),
-      pathFor('subdir/subdirFile2.txt')
-    ]);
-    dependencies.setDependencies(pathFor('subdir/subdirFile1.txt'), [
-      pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile2.txt'),
-      pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt')
-    ]);
-    dependencies.seal();
-    dependencies.captureDependencyState();
+    let transientFile = pathFor(DEP_FIXTURE_DIR, 'subdir/tmpFile1.txt');
+    try {
+      touch(transientFile, "transient\n");
+      let dependencies = new Dependencies(DEP_FIXTURE_DIR);
+      dependencies.setDependencies(pathFor('file1.txt'), [
+        pathFor('subdir/subdirFile1.txt'),
+        pathFor('subdir/subdirFile2.txt'),
+        pathFor('subdir/tmpFile1.txt'),
+        pathFor(EXT_DEP_FIXTURE_DIR, 'dep-2.txt')
+      ]);
+      dependencies.setDependencies(pathFor('subdir/subdirFile1.txt'), [
+        pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile2.txt'),
+        pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt'),
+        pathFor(EXT_DEP_FIXTURE_DIR, 'dep-2.txt')
+      ]);
+      dependencies.seal();
+      dependencies.captureDependencyState();
 
-    // If nothing has changed, no files are invalidated
-    let invalidated = dependencies.getInvalidatedFiles();
-    assert.deepEqual(invalidated, []);
+      // If nothing has changed, no files are invalidated
+      let invalidated = dependencies.getInvalidatedFiles();
+      assert.deepEqual(invalidated, []);
 
-    // only the dependent files are invalidated
-    touch(pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile1.txt'));
-    invalidated = dependencies.getInvalidatedFiles();
-    assert.deepEqual(invalidated, [
-      pathFor('file1.txt'),
-    ]);
+      // a timestamp change doesn't invalidate a local path
+      touch(pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile1.txt'));
+      invalidated = dependencies.getInvalidatedFiles();
+      assert.deepEqual(invalidated, []);
 
-    // Invalidations are reset after each call
-    invalidated = dependencies.getInvalidatedFiles();
-    assert.deepEqual(invalidated, []);
+      // in the local directory tree, content changes cause invalidation
+      touch(transientFile, "added stuff\n");
+      invalidated = dependencies.getInvalidatedFiles();
+      assert.deepEqual(invalidated, [
+        pathFor('file1.txt')
+      ]);
 
-    touch(pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt'));
-    invalidated = dependencies.getInvalidatedFiles();
-    assert.deepEqual(invalidated, [
-      pathFor('subdir/subdirFile1.txt'),
-    ]);
+      // Invalidations are reset after each call
+      invalidated = dependencies.getInvalidatedFiles();
+      assert.deepEqual(invalidated, []);
 
-    // if there's several dependent files, all are invalidated
-    touch(pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile2.txt'));
-    invalidated = dependencies.getInvalidatedFiles();
-    assert.deepEqual(invalidated.sort(), [
-      pathFor('file1.txt'),
-      pathFor('subdir/subdirFile1.txt')
-    ].sort());
+      // in external directories timestamp invalidation is used.
+      touch(pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt'));
+      invalidated = dependencies.getInvalidatedFiles();
+      assert.deepEqual(invalidated, [
+        pathFor('subdir/subdirFile1.txt'),
+      ]);
+
+      // if there's several dependent files, all are invalidated
+      touch(pathFor(EXT_DEP_FIXTURE_DIR, 'dep-2.txt'));
+      invalidated = dependencies.getInvalidatedFiles();
+      assert.deepEqual(invalidated.sort(), [
+        pathFor('file1.txt'),
+        pathFor('subdir/subdirFile1.txt')
+      ].sort());
+    } finally {
+      fs.unlinkSync(transientFile);
+    }
   })
 
   it('can serialize and deserialize', function () {
     let dependencies = new Dependencies(DEP_FIXTURE_DIR);
     dependencies.setDependencies(pathFor('file1.txt'), [
       pathFor('subdir/subdirFile1.txt'),
-      pathFor('subdir/subdirFile2.txt')
+      pathFor('subdir/subdirFile2.txt'),
     ]);
     dependencies.setDependencies(pathFor('subdir/subdirFile1.txt'), [
       pathFor(DEP_FIXTURE_DIR, 'subdir/subdirFile2.txt'),
@@ -196,10 +229,13 @@ describe('Dependency Invalidation', function() {
         pathFor(EXT_DEP_FIXTURE_DIR, 'dep-1.txt'),
       ]
     });
-    assert.deepEqual(data.fsTrees.length, 1);
-    assert.deepEqual(data.fsTrees[0].fsRoot, FS_ROOT);
-    let fileEntries = data.fsTrees[0].entries.filter((e) => !e.relativePath.endsWith('/'));
-    assert.deepEqual(fileEntries.length, 3);
+    assert.deepEqual(data.fsTrees.length, 2);
+    assert.deepEqual(data.fsTrees[0].fsRoot, DEP_FIXTURE_DIR);
+    assert.deepEqual(data.fsTrees[1].fsRoot, FS_ROOT);
+    let localFileEntries = data.fsTrees[0].entries.filter((e) => !e.relativePath.endsWith('/'));
+    assert.deepEqual(localFileEntries.length, 2);
+    let fileEntries = data.fsTrees[1].entries.filter((e) => !e.relativePath.endsWith('/'));
+    assert.deepEqual(fileEntries.length, 1);
     let json = JSON.stringify(data);
     let restoredDependencies = Dependencies.deserialize(JSON.parse(json));
     let invalidated = restoredDependencies.getInvalidatedFiles();
